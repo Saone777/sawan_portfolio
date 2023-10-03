1ff8f2a85cff3a93fbee837d179b432028a02cea
Sawan Kumar <Saone775071@gmail.com>
2023-10-03 16:23:26 +0530

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav= document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item'); //closing tag cross
 
 //Set intial state of menu
 let showMenu = false;
